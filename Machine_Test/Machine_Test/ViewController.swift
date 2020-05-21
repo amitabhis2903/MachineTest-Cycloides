a5db99c415cf8baa964f0ab7cafabf5dee17bbe7
Amitabh Pandey <amitpandey-tcs-93085@Amits-MacBook-Air.local>
2020-05-21 23:24:57 +0530

    intial commit

---
+++ Machine_Test/Machine_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Machine_Test
+//
+//  Created by Amitabh Pandey on 21/05/20.
+//  Copyright © 2020 AmitabhPandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
